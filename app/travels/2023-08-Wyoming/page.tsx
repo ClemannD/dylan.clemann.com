6d3290ba031656006cd7a76cf455943e657d41a3
Dylan Clemann <dclemann@Dylans-MBP.attlocal.net>
2023-11-19 12:11:27 -0500

    start work on wyoming2

--- app/travels/2023-08-Wyoming/page.tsx
+++ app/travels/2023-08-Wyoming/page.tsx
@@ -5,7 +5,7 @@ export default function WyomingPage() {
     return (
         <div className="">
             <section className="w-full bg-white">
-                <div className="container mx-auto py-8 px-4 lg:py-20">
+                <div className="container mx-auto py-8 px-4 lg:py-20 lg:px-0">
                     <div className="mb-10 flex justify-between">
                         <div className="">
                             <div className="text-medium mb-2 flex w-fit items-center gap-2 rounded-lg bg-slate-100 px-3 py-0.5 font-medium tracking-tight text-black">
@@ -32,52 +32,45 @@ export default function WyomingPage() {
                         totalTime="5:01:43"
                     />
 
-                    <div className="relative h-[1000px] w-full">
-                        <Image
-                            className="object-cover"
-                            src={
-                                'http://clemann.app/2023/wyoming/lake_of_crags1.jpeg'
-                            }
-                            alt={'Lake of the Crags'}
-                            fill
-                            placeholder="blur"
-                            blurDataURL={
-                                'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPMrpx0BgAFEQJETLQTEwAAAABJRU5ErkJggg=='
-                            }
-                            quality={100}
-                        />
-                    </div>
+                    <Image
+                        src={
+                            'http://clemann.app/2023/wyoming/lake-of-the-crags1.jpeg'
+                        }
+                        alt={'Lake of the Crags'}
+                        sizes="100vw"
+                        style={{
+                            width: '100%',
+                            height: 'auto',
+                        }}
+                        width={500}
+                        height={300}
+                        placeholder="blur"
+                        blurDataURL={
+                            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPMrpx0BgAFEQJETLQTEwAAAABJRU5ErkJggg=='
+                        }
+                        quality={100}
+                    />
+
+                    <Image
+                        src={
+                            'http://clemann.app/2023/wyoming/lake_of_crags1.jpeg'
+                        }
+                        alt={'Lake of the Crags'}
+                        sizes="100vw"
+                        style={{
+                            width: '100%',
+                            height: 'auto',
+                        }}
+                        width={500}
+                        height={300}
+                        placeholder="blur"
+                        blurDataURL={
+                            'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPMrpx0BgAFEQJETLQTEwAAAABJRU5ErkJggg=='
+                        }
+                        quality={100}
+                    />
                 </div>
             </section>
-            <div>
-                {/* <video controls width="500">
-                    <source
-                        src="http://clemann.app/2023/wyoming/IMG_3191-smaller.mov"
-                        type="video/mp4"
-                    />
-                </video> */}
-
-                {/* <img
-                    // crossOrigin="anonymous"
-                    // referrerPolicy="no-referrer"
-
-                    className="border"
-                    src="http://clemann.app/2023/wyoming/lake_of_crags1.jpeg"
-                    height={300}
-                    width={300}
-                /> */}
-
-                {/* <Image
-                    className="object-cover"
-                    src={'http://clemann.app/2023/wyoming/lake_of_crags1.jpeg'}
-                    fill
-                    placeholder="blur"
-                    blurDataURL={
-                        'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPMrpx0BgAFEQJETLQTEwAAAABJRU5ErkJggg=='
-                    }
-                    quality={100}
-                /> */}
-            </div>
         </div>
     );
 }

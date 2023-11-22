671c061ea51047b482beac101105a3bc7173502f
Dylan Clemann <dclemann@Dylans-MBP.attlocal.net>
2023-11-22 08:50:04 -0500

    media carousel3

--- app/travels/components/media-carousel.component.tsx
+++ app/travels/components/media-carousel.component.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import { cn } from '../../../lib/contentful/utils/cn';
+import { blurImage } from './blur-image';
 
 export default function MediaCarousel({
     media,
@@ -10,30 +11,35 @@ export default function MediaCarousel({
     media: { url: string; description?: string; type: 'IMAGE' | 'VIDEO' }[];
 }) {
     const [selectedMediaIndex, setSelectedMediaIndex] = useState(0);
-    const selectedMedia = media[selectedMediaIndex];
+    // const selectedMedia = media[selectedMediaIndex];
 
     return (
         <div className="flex h-full min-h-[560px] w-full flex-col gap-3 lg:h-[860px] lg:flex-row">
             <div className="relative h-auto flex-1 lg:h-full">
-                {
-                    // media.map((mediaItem, index) =>
-                    selectedMedia.type === 'IMAGE' ? (
+                {media.map((mediaItem, index) =>
+                    mediaItem.type === 'IMAGE' ? (
+                        // selectedMedia.type === 'IMAGE' ? (
                         <Image
-                            key={selectedMedia.url}
-                            src={selectedMedia.url}
-                            alt={selectedMedia.description || ''}
+                            // key={selectedMedia.url}
+                            // src={selectedMedia.url}
+                            // alt={selectedMedia.description || ''}
+                            key={mediaItem.url}
+                            src={mediaItem.url}
+                            alt={mediaItem.description || ''}
                             fill
+                            placeholder="blur"
+                            blurDataURL={blurImage}
                             className={cn(
-                                'h-auto w-full object-contain '
-                                // index === selectedMediaIndex
-                                //     ? 'block'
-                                //     : 'hidden'
+                                'h-auto w-full object-contain ',
+                                index === selectedMediaIndex
+                                    ? 'block'
+                                    : 'hidden'
                             )}
                             quality={100}
                         />
                     ) : (
                         <video
-                            key={selectedMedia.url}
+                            key={mediaItem.url}
                             controls
                             className="h-full w-full object-contain"
                             playsInline
@@ -41,11 +47,10 @@ export default function MediaCarousel({
                             disableRemotePlayback
                             controlsList="nodownload"
                         >
-                            <source src={selectedMedia.url} type="video/mp4" />
+                            <source src={mediaItem.url} type="video/mp4" />
                         </video>
                     )
-                    // )
-                }
+                )}
             </div>
 
             <div className="flex flex-row flex-wrap gap-3 lg:flex-col">
@@ -53,13 +58,14 @@ export default function MediaCarousel({
                     <div
                         key={mediaItem.url}
                         onClick={() => setSelectedMediaIndex(index)}
+                        onMouseOver={() => setSelectedMediaIndex(index)}
                         className="cursor-pointer"
                     >
                         <Image
                             src={mediaItem.url}
                             alt={mediaItem.description || ''}
-                            height="100"
-                            width="100"
+                            height="120"
+                            width="120"
                             className={cn(
                                 'object-cover',
                                 selectedMediaIndex === index

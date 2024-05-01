2590a3807c86eccf566ea142548c0d421979d85b
Dylan Clemann <dev@clemann.com>
2024-05-01 07:18:40 -0400

    use next Script tag for Umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import { Montserrat } from 'next/font/google';
 import Footer from './components/footer';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 // If loading a variable font, you don't need to specify the font weight
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -17,11 +18,11 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <script
-        defer
+
+      <Script
         src="https://umami.clemann.app/script.js"
         data-website-id="442c8d1d-e081-4a58-b2cb-320a4d33df1f"
-      ></script>
+      />
       <head />
       <body className={montserrat.className}>
         {children}

95a65683e4de2a6f9752237da1b5efaaafd204b3
Dylan Clemann <dev@clemann.com>
2024-05-14 19:19:07 -0400

    update url for images2

--- next.config.js
+++ next.config.js
@@ -1,24 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        unoptimized: true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.ctfassets.net',
-            },
-            {
-                protocol: 'https',
-                hostname: 'clemann.app',
-            },
-            {
-                protocol: 'http',
-                hostname: 'clemann.app',
-            },
-        ],
-    },
+  images: {
+    unoptimized: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'clemann.app',
+      },
+      {
+        protocol: 'http',
+        hostname: 'clemann.app',
+      },
+      {
+        protocol: 'https',
+        hostname: 'travels.clemann.app',
+      },
+      {
+        protocol: 'http',
+        hostname: 'travels.clemann.app',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
